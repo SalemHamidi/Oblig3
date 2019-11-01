776110ac321735a7cb0753c377b19b2533ba3518
SalemHamidi <43269413+SalemHamidi@users.noreply.github.com>
2019-11-01 20:03:28 +0100

    Grener-metode - oppgave 7 - fullført - Kompendiet 5.3.1

--- src/com/oblig3/ObligSBinTre.java
+++ src/com/oblig3/ObligSBinTre.java
@@ -349,13 +349,62 @@ public class ObligSBinTre<T> implements Beholder<T> {
   }
   
   public String lengstGren() {
-    throw new UnsupportedOperationException("Ikke kodet ennå!");
+      Node<T> p = rot;
+      StringBuilder s = new StringBuilder();
+
+      if(tom()) {
+          return "[]";
+      }
+      s.append("[");
+      if(antall == 1) {
+          s.append(p.verdi);
+      }
+
+      s.append("]");
+      return toString();
   }
   
   public String[] grener() {
-      throw new UnsupportedOperationException("Ikke kodet ennå!");
+      Liste<String> liste = new TabellListe<>();
+      StringBuilder s = new StringBuilder("[");
+
+      if (!tom()) {
+          grener(rot, liste, s);
+      }
+
+      String[] grener = new String[liste.antall()];           // oppretter tabell
+
+      int i = 0;
+
+      for (String gren : liste) {
+          grener[i++] = gren;                   // fra liste til tabell
+      }
+      return grener;                          // returnerer tabellen
   }
-  
+
+    private void grener(Node<T> p, Liste<String> liste, StringBuilder s) {
+        T verdi = p.verdi;
+        int k = verdi.toString().length(); // lengden på verdi
+
+        if (p.høyre == null && p.venstre == null) {  // bladnode
+
+            liste.leggInn(s.append(verdi).append(']').toString());
+
+            // må fjerne det som ble lagt inn sist - dvs. k + 1 tegn
+            s.delete(s.length() - k - 1, s.length());
+        }
+        else {
+            s.append(p.verdi).append(',').append(' ');  // legger inn k + 2 tegn
+            if (p.venstre != null) {
+                grener(p.venstre, liste, s);
+            }
+            if (p.høyre != null) {
+                grener(p.høyre, liste, s);
+            }
+            s.delete(s.length() - k - 2, s.length());   // fjerner k + 2 tegn
+        }
+    }
+
   public String bladnodeverdier() {
     throw new UnsupportedOperationException("Ikke kodet ennå!");
   }
